81c104133b45df42d0e33cdcab8a42b1e107ffb2
GekiteniusY <genkichi.yogi@gmail.com>
2024-01-04 13:27:28 +0900

    画面の一部を更新するように修正

--- hackernews-app/app/top20/[id]/page.js
+++ hackernews-app/app/top20/[id]/page.js
@@ -1,11 +1,19 @@
 import "../_style/article.css";
 import { getItem } from "../../_utils/hackerNews";
+import Divider from "@mui/material/Divider";
 
 export default async function Top20IdPage({ params }) {
   // paramsからURLのパラメータを取り出す
   const { id } = params;
   // 記事データを取得する
   const item = await getItem(id);
+
+  // 記事データ内に含まれるコメントIDを１件ずつ取得する
+  const kids = await Promise.all(
+    item.kids.map((kidsItem) => getItem(kidsItem))
+  );
+
+  //   console.log(kids);
 
   return (
     <article>
@@ -16,6 +24,16 @@ export default async function Top20IdPage({ params }) {
       <p>
         <a href={item.url}>{item.url}</a>
       </p>
+      <hr />
+      <h2>Comments</h2>
+      {kids.map((kidsItem) => (
+        <div key={kidsItem.id}>
+          <h3>by: {kidsItem.by}</h3>
+          <p>{kidsItem.text}</p>
+          <p>{new Date(kidsItem.time * 1000).toLocaleString()}</p>
+          <hr />
+        </div>
+      ))}
     </article>
   );
 }
